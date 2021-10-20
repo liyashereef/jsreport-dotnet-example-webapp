a129cfa253ccc32fbae3515ef6d55fe7b7ec6387
Rahul Vijayakumar <rahul.vijayakumar@secture.ca>
2021-10-20 13:23:34 +0530

    Added S3 configurations.

--- config/filesystems.php
+++ config/filesystems.php
@@ -17,6 +17,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Many applications store files both locally and in the cloud. For this
+    | reason, you may specify a default "cloud" driver here. This driver
+    | will be bound as the Cloud disk implementation in the container.
+    |
+    */
+
+    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
     |
@@ -48,6 +61,37 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        ],
+        'awsS3Bucket' => [
+            'driver' => 's3',
+            'key'    => env('AWS_KEY', ''),
+            'secret' => env('AWS_SECRET', ''),
+            'region' => env('S3_REGION', ''),
+            'bucket' => env('S3_BUCKET', ''),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        ],
+
+        's3meet' => [
+            'driver' => 's3',
+            'key' => env('AWS_MEETACCESS_KEY_ID'),
+            'secret' => env('AWS_MEETSECRET_ACCESS_KEY'),
+            'region' => env('AWS_MEETDEFAULT_REGION'),
+            'bucket' => env('AWS_MEETBUCKET'),
+            'url' => env('AWS_MEETURL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        ],
+
+        's3-recruitment' => [
+            'driver' => 's3',
+            'key' => env('AWS_KEY'),
+            'secret' => env('AWS_SECRET'),
+            'region' => 'us-east-2',
+            'bucket' => env('AWS_RECRUITMENT_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
