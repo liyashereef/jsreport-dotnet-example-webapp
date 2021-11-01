f6f149a3bdaefec999acc1078c4c43e946fc6f33
reshmarajan93 <reshma.rajan@secture.ca>
2021-11-01 18:04:28 +0530

    bug fix due laarvel8 feature in customer details

--- Modules/Supervisorpanel/Resources/views/partials/leavetype.blade.php
+++ Modules/Supervisorpanel/Resources/views/partials/leavetype.blade.php
@@ -12,13 +12,13 @@
         @foreach($employee_list as $each_employee_id => $each_empolyee)
         <option
             value="{{trim($each_employee_id)}}"
-            @if(isset($employee_id) && ((trim($each_employee_id) == $employee_id) ||(trim($each_empolyee) == $employee_id))) selected @endif>{{$each_empolyee or ''}}</option>
+            @if(isset($employee_id) && ((trim($each_employee_id) == $employee_id) ||(trim($each_empolyee) == $employee_id))) selected @endif>{{$each_empolyee ?? ''}}</option>
         @endforeach --}}
     </select>
     </div>
-    <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Date of absenteeism</label><div class="col-sm-6 col-md-6 margin-top-1"><input type="text" class="datepicker form-control" name="{{$name}}" value="{{$date or ''}}"></div>
+    <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Date of absenteeism</label><div class="col-sm-6 col-md-6 margin-top-1"><input type="text" class="datepicker form-control" name="{{$name}}" value="{{$date ?? ''}}"></div>
     <div class="form-control-feedback"><span class="help-block text-danger align-middle font-12"></span></div>
-    <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Hours Booked Off</label><div class="col-sm-6 col-md-6 margin-top-1"><input type="number" class="form-control" name="{{$name}}" max="999.99" step="0.01" value="{{$hours_off or ''}}"></div>
+    <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Hours Booked Off</label><div class="col-sm-6 col-md-6 margin-top-1"><input type="number" class="form-control" name="{{$name}}" max="999.99" step="0.01" value="{{$hours_off ?? ''}}"></div>
 	<div class="form-control-feedback"><span class="help-block text-danger align-middle font-12"></span></div>
     <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Reason</label><div class="col-sm-6 col-md-6 margin-top-1">
         <select name="{{$name}}" class="select2-reason-list form-control" tabindex="-1">
@@ -26,28 +26,28 @@
             @foreach($leave_reason as $each_reason_id => $each_reason)
             <option
                 value="{{trim($each_reason_id)}}"
-                @if(isset($reason_id) && ((trim($each_reason_id) == $reason_id) ||(trim($each_reason) == $reason_id))) selected @endif>{{$each_reason or ''}}</option>
+                @if(isset($reason_id) && ((trim($each_reason_id) == $reason_id) ||(trim($each_reason) == $reason_id))) selected @endif>{{$each_reason ?? ''}}</option>
             @endforeach
         </select>
 	</div>
 	<div class="form-control-feedback"><span class="help-block text-danger align-middle font-12"></span></div>
-	<label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Note</label><div class="col-sm-6 col-md-6 margin-top-1"><textarea class="form-control" name="{{$name}}" maxlength="1000">{{$notes or ''}}</textarea></div><div class="form-control-feedback"><span class="help-block text-danger align-middle font-12"></span></div>
+	<label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Note</label><div class="col-sm-6 col-md-6 margin-top-1"><textarea class="form-control" name="{{$name}}" maxlength="1000">{{$notes ?? ''}}</textarea></div><div class="form-control-feedback"><span class="help-block text-danger align-middle font-12"></span></div>
 @else
     @foreach($employee_list as $each_employee_id => $each_empolyee)
         @if(isset($employee_id) && ((trim($each_employee_id) == $employee_id) ||(trim($each_empolyee) == $employee_id)))
-        <span class="view-form-element col-sm-6 col-md-6">{{$each_empolyee or ''}}</span>
+        <span class="view-form-element col-sm-6 col-md-6">{{$each_empolyee ?? ''}}</span>
         @endif
     @endforeach
     <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Date of absenteeism</label>
-    <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$date or ''}}</span>
+    <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$date ?? ''}}</span>
 	<label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Hours Booked Off</label>
-    <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$hours_off or ''}}</span>
+    <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$hours_off ?? ''}}</span>
     @foreach($leave_reason as $each_reason_id => $each_reason)
         @if(isset($reason_id) && ((trim($each_reason_id) == $reason_id) ||(trim($each_reason_id) == $reason_id)))
         <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Reason</label>
-        <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$each_reason or ''}}</span>
+        <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$each_reason ?? ''}}</span>
         @endif
     @endforeach
     <label class="col-sm-6 col-md-6 col-form-label child-align margin-top-1">Note</label>
-    <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$notes or ''}}</span>
+    <span class="view-form-element col-sm-6 col-md-6 margin-top-1">{{$notes ?? ''}}</span>
 @endif
