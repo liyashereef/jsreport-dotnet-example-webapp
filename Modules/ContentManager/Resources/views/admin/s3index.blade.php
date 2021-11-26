ddf301f5e1f68253ffd11094ca34a33be8fb7238
liyashereef <lshereef@secture.ca>
2021-11-26 11:29:13 +0530

    Uppy AppJS Issue reolved

--- Modules/ContentManager/Resources/views/admin/s3index.blade.php
+++ Modules/ContentManager/Resources/views/admin/s3index.blade.php
@@ -54,7 +54,10 @@
 
             $(document).on("click","#mdl_save_change",function(e){
                 e.preventDefault();
-                
+                $('body').loading({
+                    stoppable: false,
+                    message: 'Please wait...'
+                });
                 let valid = validateAttachment();
                 let editId=$("input[name=id]").val();
                 let message="";
@@ -67,7 +70,7 @@
                     submitForm();
                 }else{
                     swal("Warning","Please validate inputs/Click upload","warning")
-                    //$('body').loading('stop');
+                    $('body').loading('stop');
                 }
 
                 
@@ -120,7 +123,7 @@
                     },
                     success: function(data) {
                             // ... do something with the data...
-                           // $('body').loading('stop');
+                            $('body').loading('stop');
                             let routeUrl="{{route("content-manager.view")}}";
                             swal({
                                 title: "Uploaded",
@@ -298,7 +301,7 @@ id="video-results"><!-- server response here --></div>
 </form>
 @endsection
 
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/appuppy.js') }}" defer></script>
 @section('scripts')
    
 @endsection
