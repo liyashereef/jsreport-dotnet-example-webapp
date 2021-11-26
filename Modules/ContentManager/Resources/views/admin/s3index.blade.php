56cc5f65995079f4900d69c47511d224b12b7e20
liyashereef <lshereef@secture.ca>
2021-11-26 12:19:07 +0530

    fontweight

--- Modules/ContentManager/Resources/views/admin/s3index.blade.php
+++ Modules/ContentManager/Resources/views/admin/s3index.blade.php
@@ -5,6 +5,9 @@
         pointer-events: none;
 
     }
+    .add-new-label h4{
+        font-weight: bold
+    }
 </style>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
