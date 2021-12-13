667c976e60e6b1d1de4e693dbd539a82e4ba55bf
sreeharichandran <sreehari.chandran@secture.ca>
2021-12-13 13:31:49 +0530

    seeder list updation

--- Modules/Chat/Database/Seeders/ModulePermissionChatTableSeeder.php
+++ Modules/Chat/Database/Seeders/ModulePermissionChatTableSeeder.php
@@ -19,8 +19,17 @@ class ModulePermissionChatTableSeeder extends Seeder
         $view_chat_menu = HelperService::getPermissionId('view_chat_menu');
         $view_chat_history = HelperService::getPermissionId('view_chat_history');
         $view_chat_in_api = HelperService::getPermissionId('view_chat_in_api');
+        $view_all_customer_chatlist = HelperService::getPermissionId('view_all_customer_chatlist');
+        $view_allocated_customer_chatlist = HelperService::getPermissionId('view_allocated_customer_chatlist');
         $module_id = HelperService::getModuleId('Chat');
-
+         \DB::table('module_permissions')->where('module_id', $module_id)
+        ->whereIn('permission_id', [
+            $view_chat_menu,
+            $view_chat_history,
+            $view_chat_in_api,
+            $view_all_customer_chatlist,
+            $view_allocated_customer_chatlist
+        ])->delete();
         \DB::table('module_permissions')->insert(array(
             0 => array(
                 'module_id' => $module_id,
@@ -45,6 +54,22 @@ class ModulePermissionChatTableSeeder extends Seeder
                 'updated_at' => Carbon::now(),
                 'permission_id' => $view_chat_in_api,
                 'sequence_number' => 3,
+            ),
+            3 => array(
+                'module_id' => $module_id,
+                'permission_description' => 'View All Customers in Chat list',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'permission_id' => $view_all_customer_chatlist,
+                'sequence_number' => 4,
+            ),
+            4 => array(
+                'module_id' => $module_id,
+                'permission_description' => 'View Allocated Customers in Chat list',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'permission_id' => $view_allocated_customer_chatlist,
+                'sequence_number' => 5,
             ),
         ));
     }
--- Modules/Chat/Database/Seeders/RoleAndPermissionsChatTableSeeder.php
+++ Modules/Chat/Database/Seeders/RoleAndPermissionsChatTableSeeder.php
@@ -16,12 +16,21 @@ class RoleAndPermissionsChatTableSeeder extends Seeder
      */
     public function run()
     {
-        Model::unguard();
+        
+        \DB::table('permissions')->whereIn('name', [
+            'view_chat',
+            'view_chat_menu',
+            'view_chat_history',
+            'view_chat_in_api',
+            'view_all_customer_chatlist',
+            'view_allocated_customer_chatlist'
+            ])->delete();
         Permission::create(['name' => 'view_chat']);
         Permission::create(['name' => 'view_chat_menu']);
         Permission::create(['name' => 'view_chat_history']);
         Permission::create(['name' => 'view_chat_in_api']);
-       
+        Permission::create(['name' => 'view_all_customer_chatlist']);
+        Permission::create(['name' => 'view_allocated_customer_chatlist']);
         $s_admin_role = Role::findByName('super_admin');
         $s_admin_role->givePermissionTo(Permission::all());
         $admin_role = Role::findByName('admin');
